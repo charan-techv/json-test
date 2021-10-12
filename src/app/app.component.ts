9ab845d966d5faeee306f9e66b92bffb041812d0
charan kumar <charan.nandibaktula>
2021-10-12 11:10:12 +0530

    json-api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'json-test';
+
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'brewery_type',
+    'street',
+    'address_2',
+    'address_3',
+    'city',
+    'state',
+    'country_province',
+    'postal_code',
+    'country',
+    'longitude',
+    'latitude',
+    'phone',
+    'website_url',
+    'updated_at',
+    'created_at'
+  ];
+
+  dataSource: any[] = [];
+
+  constructor(private http: HttpClient) { }
+  url = "https://api.openbrewerydb.org/breweries";
+  httpData: any;
+
+  ngOnInit() {
+    this.http.get<Kafein[]>(this.url).subscribe(data => {
+      this.httpData = data;
+    })
+
+  }
+
+}
+interface Kafein {
+  name: string;
+  address: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// import { MatTableModule } from '@angular/material';
+import {MatTableModule} from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule, 
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
